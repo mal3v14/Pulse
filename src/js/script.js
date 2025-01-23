c4c954812026332580888ad2d397d2e85d7a4bf3
Malik <malikkasumov2@gmail.com>
2025-01-23 16:47:57 +0200

    replaced jq slider with new js slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,39 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        variableWidth: true,
-        // с фейдом пропадают все слайды, кроме 1
-        // fade: true,
-        // cssEase: 'linear'
-        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.svg"> </button>',
-        nextArrow: '<button type="button" class="slick-next"> <img src="icons/right.svg"> </button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
+// $(document).ready(function () {
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         variableWidth: true,
+//         // с фейдом пропадают все слайды, кроме 1
+//         // fade: true,
+//         // cssEase: 'linear'
+//         prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.svg"> </button>',
+//         nextArrow: '<button type="button" class="slick-next"> <img src="icons/right.svg"> </button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
 
-    });
+//     });
+// });
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
