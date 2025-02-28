0016c9175df7b7509c4273838fb31bbefde09cc4
Malik <malikkasumov2@gmail.com>
2025-02-28 13:51:17 +0200

    Added mailer and smart.php (not work properly)

--- src/js/script.js
+++ src/js/script.js
@@ -145,7 +145,25 @@ document.querySelector('.next').addEventListener('click', function () {
 
     $('input[name=phone]').mask("+38 (999) 999-99-99");
 
+    $('form').submit(function (e) {
+        e.preventDefault();
 
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn(slow);
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 })(jQuery);
 
 
