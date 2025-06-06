dd1ad5c4f55607d2394c65c9bcb53b789b29f689
Malik <malikkasumov2@gmail.com>
2025-06-06 04:22:35 +0300

    added deploy folder for gh-pages

---
+++ docs/js/script.js
@@ -0,0 +1,161 @@
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    preventScrollOnTouch: "auto",
+    mouseDrag: true,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+(function ($) {
+    $(function () {
+
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+            $(this)
+                .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+                .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+    });
+
+    // $('.catalog-item__link').each(function (i) {
+    //     $(this).on('click', function (e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+
+    //     })
+    // });
+
+    // $('.catalog-item__back').each(function (i) {
+    //     $(this).on('click', function (e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+
+    //     })
+    // });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+
+    // modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+
+    $(".button_mini").each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите минимум {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введён адрес почты"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+38 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 900) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("#up").on('click', function (event) {
+
+        $('html, body').animate({
+            scrollTop: $(_href).offset().top
+        }, 800, function () {
+
+            window.location.hash = hash;
+        });
+    });
+
+    new WOW().init();
+
+})(jQuery);
+
+
+
+
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,19 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const del = require('del');
 
+// Сервер для разработки
 gulp.task('server', function () {
-
     browserSync({
         server: {
             baseDir: "src"
         }
     });
-
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
+// Компиляция SASS
 gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
@@ -26,8 +27,27 @@ gulp.task('styles', function () {
         .pipe(browserSync.stream());
 });
 
+// Вотчер
 gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+});
 
+// Очистка папки docs
+gulp.task('clean', function () {
+    return del('docs');
+});
+
+// Копирование файлов в папку docs (для GitHub Pages)
+gulp.task('build', gulp.series('clean', 'styles', function () {
+    return gulp.src([
+        'src/*.html',
+        'src/css/**/*',
+        'src/js/**/*',
+        'src/img/**/*',
+        'src/fonts/**/*'
+    ], { base: 'src' })
+        .pipe(gulp.dest('docs'));
+}));
+
+// По умолчанию
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
